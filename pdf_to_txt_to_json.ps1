64156675eeadb0706f6880091058707ae04a3a80
siliciium <103604296+siliciium@users.noreply.github.com>
2023-11-22 23:52:20 +0100

    Create pdf_to_txt_to_json.ps1

---
+++ pdf_to_txt_to_json.ps1
@@ -0,0 +1,32 @@
+$list = Get-Content -Encoding Utf8 -Path "usb_vids_080223.txt"
+
+$obj = [ordered]@{}
+
+foreach($line in $list.Split([System.Environment]::NewLine)){
+   
+    if(-not [string]::IsNullOrEmpty($line)){
+
+        $matches = [regex]::Matches($line, '^((.*)(\s+)([0-9]{1,6}))$')   
+        if($matches.Groups.Count -eq 5){
+
+            $manufacturer = $matches.Groups[2].Value.Trim()
+            $vid = [convert]::ToUInt32($matches.Groups[4].Value)
+
+            #Write-Host -ForegroundColor Gray "[add] $manufacturer $vid"  
+
+            if(-not $obj.PSobject.Properties.name.Contains($manufacturer)){
+                $obj["$manufacturer"] = $vid 
+            }else{
+                Write-Host -ForegroundColor Yellow "[skip] $line"  
+            }
+
+
+        }else{
+            Write-Host -ForegroundColor Red "[skip] $line"
+        }
+
+    }
+
+} 
+
+$obj | Sort-Object | ConvertTo-Json -Depth 10 | Out-File -Encoding utf8 -FilePath "usb_vids_080223.json"
